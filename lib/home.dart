92e0d5e37e176884abaf69985f62aab195697ff3
Aman Kumar <user2amankr@gmail.com>
2024-06-08 18:38:15 +0530

    Fifth Commit

--- lib/home.dart
+++ lib/home.dart
@@ -12,116 +12,131 @@ class HomeMain extends StatelessWidget {
       child: Scaffold(
         body: SafeArea(
           top: true,
+          // whole page
           child: Container(
             width: 1438,
             height: 896,
-             
-
             child: Padding(
-                  padding: EdgeInsets.all(80),
-            child: Container(
-                width: 1438,
-                height: 896,
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    // Text("this")
-                    Container(
-                      // navbar
-                      decoration: BoxDecoration(
-                        color: Color(0x6EE8E5E5),
-                      ),
-                      child: Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
-                          child: Row(
-                            children: [
-                              Row(
-                                // Navbar
-                                children: [
-                                  // Foodiee
-                                  const Padding(
-                                    padding: EdgeInsetsDirectional.fromSTEB(
-                                        0, 10, 0, 0),
-                                    child: Row(
-                                      // Foodie Row
-                                      mainAxisSize: MainAxisSize.max,
-
-                                      children: [
-                                        Text(
-                                          'Foodiee',
-                                          style: TextStyle(
-                                            fontSize: 30,
-                                            letterSpacing: 0,
-                                            fontWeight: FontWeight.w600,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  //  Foodiee
-
-                                  // Home Menu Pricing About Us
-                                  const Padding(
-                                    padding: EdgeInsetsDirectional.fromSTEB(
-                                        0, 18, 0, 0),
-                                    child: Row(
-                                      children: [
-                                        Text('Home',
-                                            style: TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.w500)),
-                                        Text('Menu',
-                                            style: TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.w500)),
-                                        Text('Pricing',
-                                            style: TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.w500)),
-                                        Text('About Us',
-                                            style: TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.w500)),
-                                      ],
-                                    ),
-                                  ),
-
-                                  // Home Menu Pricing About Us
-
-                                  Row(
+                padding: EdgeInsets.all(80),
+                // inner container
+                child: Container(
+                    width: 1438,
+                    height: 896,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        // navbar
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Color(0x6EE8E5E5),
+                          ),
+                          child: Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+              //nav-bar
+                            child: Row(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+               // Foodiee               
+                                Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 10, 0, 0),
+                                  child: Row(
                                     mainAxisSize: MainAxisSize.max,
                                     children: [
-                                      Container(
-                                        width: 161,
-                                        height: 66,
-                                        decoration: BoxDecoration(
-                                          color: Color(0xD0EF5C15),
-                                          borderRadius:
-                                              BorderRadius.circular(20),
-                                        ),
-                                        child: const Padding(
-                                          padding: EdgeInsets.all(20),
-                                          child: Text(
-                                            ' Contact Us',
-                                            style: TextStyle(
-                                              color: Color(0xFFFEFEFE),
-                                              fontSize: 20,
+                                      Text(
+                                        'Foodiee',
+                                        style: TextStyle(
+                                              fontFamily: 'Outfit',
+                                              fontSize: 30,
                                               letterSpacing: 0,
+                                              fontWeight: FontWeight.w600,
                                             ),
-                                          ),
-                                        ),
                                       ),
                                     ],
                                   ),
-                                ],
-                              ) //navbar
-                            ],
-                          )),
-                    )
-                  ],
-                )
-                )
-                ),
+                                ),
+                // Foodiee                
+                                Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 18, 0, 0),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.max,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Home',
+                                        style: TextStyle(
+                                              fontFamily: 'Readex Pro',
+                                              fontSize: 20,
+                                              letterSpacing: 0,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                      ),
+                                      Text(
+                                        'Menu',
+                                        style: TextStyle(
+                                              fontFamily: 'Readex Pro',
+                                              fontSize: 20,
+                                              letterSpacing: 0,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                      ),
+                                      Text(
+                                        'Pricing',
+                                        style: TextStyle(
+                                              fontFamily: 'Readex Pro',
+                                              fontSize: 20,
+                                              letterSpacing: 0,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                      ),
+                                      Text(
+                                        'About Us',
+                                        style: TextStyle(
+                                              fontFamily: 'Readex Pro',
+                                              fontSize: 20,
+                                              letterSpacing: 0,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    Container(
+                                      width: 161,
+                                      height: 66,
+                                      decoration: BoxDecoration(
+                                        color: Color(0xD0EF5C15),
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                      child: Padding(
+                                        padding: EdgeInsets.all(20),
+                                        child: Text(
+                                          ' Contact Us',
+                                          style: TextStyle(
+                                                fontFamily: 'Readex Pro',
+                                                color: Color(0xFFFEFEFE),
+                                                fontSize: 20,
+                                                letterSpacing: 0,
+                                              ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ))),
           ),
         ),
       ),
