2727486079c7061292c8c1bd53b9857110d0092e
Aman Kumar <user2amankr@gmail.com>
2024-06-08 15:51:21 +0530

    Fourth Commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:excel/excel.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 class HomeMain extends StatelessWidget {
@@ -11,36 +13,116 @@ class HomeMain extends StatelessWidget {
         body: SafeArea(
           top: true,
           child: Container(
-              width: 400,
-              height: 400,
-              child: Column(
-                children: [
-                  // Text("this")
-                  Container(
-                      child: Row(
+            width: 1438,
+            height: 896,
+             
 
-                    children: [
-                     Row(
-                        children: [
+            child: Padding(
+                  padding: EdgeInsets.all(80),
+            child: Container(
+                width: 1438,
+                height: 896,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    // Text("this")
+                    Container(
+                      // navbar
+                      decoration: BoxDecoration(
+                        color: Color(0x6EE8E5E5),
+                      ),
+                      child: Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                          child: Row(
+                            children: [
+                              Row(
+                                // Navbar
+                                children: [
+                                  // Foodiee
+                                  const Padding(
+                                    padding: EdgeInsetsDirectional.fromSTEB(
+                                        0, 10, 0, 0),
+                                    child: Row(
+                                      // Foodie Row
+                                      mainAxisSize: MainAxisSize.max,
 
-                        Row(
-                                   
+                                      children: [
+                                        Text(
+                                          'Foodiee',
+                                          style: TextStyle(
+                                            fontSize: 30,
+                                            letterSpacing: 0,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  //  Foodiee
+
+                                  // Home Menu Pricing About Us
+                                  const Padding(
+                                    padding: EdgeInsetsDirectional.fromSTEB(
+                                        0, 18, 0, 0),
+                                    child: Row(
+                                      children: [
+                                        Text('Home',
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w500)),
+                                        Text('Menu',
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w500)),
+                                        Text('Pricing',
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w500)),
+                                        Text('About Us',
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w500)),
+                                      ],
+                                    ),
+                                  ),
+
+                                  // Home Menu Pricing About Us
+
+                                  Row(
+                                    mainAxisSize: MainAxisSize.max,
                                     children: [
-                                      Text(
-                                        'Foodiee',
-                                        style : TextStyle()
-                                         
+                                      Container(
+                                        width: 161,
+                                        height: 66,
+                                        decoration: BoxDecoration(
+                                          color: Color(0xD0EF5C15),
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                        ),
+                                        child: const Padding(
+                                          padding: EdgeInsets.all(20),
+                                          child: Text(
+                                            ' Contact Us',
+                                            style: TextStyle(
+                                              color: Color(0xFFFEFEFE),
+                                              fontSize: 20,
+                                              letterSpacing: 0,
+                                            ),
+                                          ),
+                                        ),
                                       ),
                                     ],
                                   ),
-                        
-                        ],
-                      )
-                    ],
-                  )
-                  )
-                ],
-              )),
+                                ],
+                              ) //navbar
+                            ],
+                          )),
+                    )
+                  ],
+                )
+                )
+                ),
+          ),
         ),
       ),
     );
